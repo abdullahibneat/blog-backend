b81aa9d7162e607a4e13ea614a06718c089b1d19
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-06 12:02:13 +0100

    Part 4.4: Implement function to calculate total number of likes, add test

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,7 @@
-const dummy = (blogs) => 1
+const totalLikes = (blogs) => {
+    blogs = blogs.map(b => b.likes)
+    const sum = blogs.reduce((sum, item) => sum+item, 0)
+    return blogs.length === 0? 0 : sum
+}
 
-module.exports = { dummy }
+module.exports = { totalLikes }
