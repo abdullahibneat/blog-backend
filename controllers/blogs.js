5c087a5e2118fd55cae7ca82367c38dcf377ca8e
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-08 14:37:55 +0100

    Part 4.10: Refactor POST /api/blogs to use async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -5,13 +5,11 @@ blogsRouter.get("/", async (req, res) => {
     res.json(await Blog.find({}))
 })
 
-blogsRouter.post("/", (req, res, next) => {
+blogsRouter.post("/", async (req, res, next) => {
     const blog = new Blog(req.body)
 
-    blog
-        .save()
-        .then(result => res.status(201).json(result))
-        .catch(err => next(err))
+    try { res.status(201).json(await blog.save()) }
+    catch(err) { next(err) }
 })
 
 module.exports = blogsRouter
