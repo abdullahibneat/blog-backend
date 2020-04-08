4c267493a76ad03a714e9f2e86a4f712c8be5b17
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-08 13:01:56 +0100

    toJSON(): Rename _id to id, remove __v

--- models/blog.js
+++ models/blog.js
@@ -18,6 +18,14 @@ const blogSchema = mongoose.Schema({
     }
 })
 
+blogSchema.set("toJSON", {
+    transform: (doc, ret) => {
+        ret.id = ret._id.toString()
+        delete ret._id
+        delete ret.__v
+    }
+})
+
 const Blog = mongoose.model("Blog", blogSchema)
 
 module.exports = Blog
