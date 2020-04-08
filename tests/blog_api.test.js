a7d8478e536e001cf8c730f48c28f4fb54a670de
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-08 13:40:09 +0100

    Part 4.9: Test that Blog objects have the ID property

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,13 +4,13 @@ const mongoose = require("mongoose")
 const Blog = require("../models/blog")
 const helper = require("./test_helper")
 
-const blogs = [...helper.sample_blogs].map(b => new Blog(b))
+const blogs = [...helper.sample_blogs]
 
 const api = supertest(app)
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    await Promise.all(blogs.map(b => b.save()))
+    await Promise.all(blogs.map(b => new Blog(b).save()))
 })
 
 describe("Testing API", () => {
@@ -20,6 +20,13 @@ describe("Testing API", () => {
             .expect(200)
             .expect("Content-Type", /application\/json/)
         expect(result.body.length).toBe(blogs.length)
+    })
+
+    test("Blogs have an ID field", async () => {
+        const result = await api.get("/api/blogs")
+        result.body.forEach(b => {
+            expect(b.id).toBeDefined()
+        })
     })
 })
 
