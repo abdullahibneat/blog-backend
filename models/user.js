17114bddf9ffb8c4bdd8154ba1df15c5b21c8512
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-09 19:23:46 +0100

    Prepare user schema for authentication

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    }
+})
+
+userSchema.set("toJSON", {
+    transform: (doc, ret) => {
+        ret.id = ret._id.toString()
+        delete ret._id
+        delete ret.__v
+        delete ret.passwordHash
+    }
+})
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User
