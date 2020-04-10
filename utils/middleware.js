4026be37e66c7b8510dea600140d9ca1c2a1c65b
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-10 12:10:21 +0100

    Remove unused "next"

--- utils/middleware.js
+++ utils/middleware.js
@@ -8,7 +8,7 @@ const requestLogger = (req, res, next) => {
     next()
 }
 
-const unknownEndpoint = (req, res, next) => {
+const unknownEndpoint = (req, res) => {
     res.status(404).send({ error: "Unknown endpoint" })
 }
 
