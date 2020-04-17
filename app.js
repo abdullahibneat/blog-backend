add7dd26d8e0d1771f3572b38f289fd492f6ae99
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-17 11:45:56 +0100

    Implement /testing/reset endpoint to reset database in test environment

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(bodyParser.json())
 app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 
+if(process.env.NODE_ENV === "test") {
+    const testingRouter = require("./controllers/testing")
+    app.use("/api/testing", testingRouter)
+}
+
 app.use("/api/login", loginRouter)
 app.use("/api/users", usersRouter)
 app.use("/api/blogs", blogsRouter)
---
+++ controllers/testing.js
@@ -0,0 +1,11 @@
+const testingRouter = require("express").Router()
+const User = require("../models/user")
+const Blog = require("../models/blog")
+
+testingRouter.post("/reset", async (req, res) => {
+    await User.deleteMany({})
+    await Blog.deleteMany({})
+    res.sendStatus(204)
+})
+
+module.exports = testingRouter
